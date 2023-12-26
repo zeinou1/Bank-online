e9341385e0eedda1f0c65f608b991139d7377d03
abdoulkader <zarikader@gmail.com>
2023-12-26 13:39:04 +0100

    Add project

---
+++ src/assets/store.js
@@ -0,0 +1,10 @@
+import {configureStore} from "@reduxjs/toolkit";
+import counter from "../features/counter.js";
+
+export const store = configureStore({
+    reducer: {
+        counter,
+
+    },
+
+})
---
+++ src/features/counter.js
@@ -0,0 +1,17 @@
+import {createSlice} from "@reduxjs/toolkit";
+const initialState = {
+    value : 0
+}
+export const  counter = createSlice( {
+    name : "counter",
+    initialState,
+    reducers: {
+        // action reducer
+increment : (state, action) => {
+    state.value++
+}
+    }
+})
+console.log(counter)
+export const {increment } = counter.actions
+export default counter.reducer
