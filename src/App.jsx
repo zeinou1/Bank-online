19cf33f6cdeaf593d49e1205c38ab0a1df5660f0
abdoulkader <zarikader@gmail.com>
2024-01-04 19:42:48 +0100

    MA

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navigation from "../Components/Common/Navigation.jsx";
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Navigation/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Common/Navigation.jsx
@@ -0,0 +1,28 @@
+
+import logo from "./../../assets/Logo.png"
+import {Link} from 'react-router-dom'
+import "./../../styles/common/styles.scss"
+function Navigation() {
+    return (
+        <nav className="main-nav">
+            <Link className="main-nav-logo" to="/">
+                <img
+                    className="main-nav-logo-image"
+                    src={logo}
+                    alt="Argent Bank Logo"
+                />
+                <h1 className="sr-only">Argent Bank</h1>
+            </Link>
+
+            <div>
+                <Link className="main-nav-item" to="/SingIn">
+                    <i className="fa fa-user-circle"></i>
+                    Sign In
+                </Link>
+            </div>
+        </nav>
+)
+    ;
+}
+
+export default Navigation;
