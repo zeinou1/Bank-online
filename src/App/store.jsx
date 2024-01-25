a0380c5f2d563155e15e9d8efe16c67f179d35a3
abdoulkader <zarikader@gmail.com>
2024-01-25 15:39:39 +0100

    Ma store

--- src/App/store.jsx
+++ src/App/store.jsx
@@ -1,24 +1,12 @@
-import { configureStore,combineReducers } from "@reduxjs/toolkit";
-import userReducer from './../features/LoginSlice';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import userReducer from "./../features/LoginSlice";
 import ProfileReducer from "../features/profileSlice";
 //const state = {}
 
 const store = configureStore({
-    reducer: {
-        user: userReducer,
-        profile: ProfileReducer
-    }
-})
-
-
-// const NosReducers = combineReducers({
-//         user: userReducer,
-//         profile: ProfileReducer
-// });
-
-
-// const store = configureStore({
-//     preloadedState: state,
-//     reducer: NosReducers,
-// })
- export default store;
+  reducer: {
+    user: userReducer,
+    profile: ProfileReducer,
+  },
+});
+export default store;
